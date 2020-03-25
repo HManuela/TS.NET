52d2970a337f3ac19fc689f30e1b3b3f9c545ea1
HManuela <manuela.horduna@gmail.com>
2020-03-25 22:05:33 +0200

    Add files via upload

---
+++ TS_lab1/TS_lab1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pub p = new Pub();
+
+            p.OnChange += () => Console.WriteLine("Subscriber 1!");
+
+            p.OnChange += () => Console.WriteLine("Subscriber 2!");
+
+
+            p.Raise();
+
+            //After this Raise() method is called
+            //all subscribers callback methods will get invoked
+
+            Console.WriteLine("Press enter to terminate!");
+            Console.ReadLine();
+
+        
+    }
+    }
+
+
+    public class Pub
+    {
+        //OnChange property containing all the 
+        //list of subscribers callback methods
+        public Action OnChange { get; set; }
+
+        public void Raise()
+        {
+            //Check if OnChange Action is set before invoking it
+            if (OnChange != null)
+            {
+                //Invoke OnChange Action
+                OnChange();
+            }
+        }
+    }
+
+
+
+}
+
---
+++ TS_lab1/TS_lab1/prim.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS_lab1
+{
+    class prim
+    {
+        static void Main(string[] args)
+        {
+            Boolean notfound = true;
+            string given = Console.ReadLine();
+            int number = Convert.ToInt32(given);
+            while (number >= 2 && notfound)
+            {
+                int i;
+                for (i = 2; i <= number / 2; i++)
+                    if (number % i == 0)
+                        break;
+                if (i == number / 2)
+                {
+                    notfound = false;
+                    Console.WriteLine(number);
+                }
+            number =number - 1;
+
+            }
+        }
+    }
+}
