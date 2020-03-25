6e119fcfcbe3ee8bee0ab96d9f2bc8f285607540
HManuela <manuela.horduna@gmail.com>
2020-03-25 22:21:08 +0200

    Add files via upload

---
+++ TS_Lab5_EF-Studies/EF/context/ProductContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Entity;
+
+namespace Studies.context
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<models.Product> Products { get; set; }
+
+        public ProductContext() : base("name=ProductContext")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<models.Product>()
+                .Map(m =>
+                {
+                    m.Properties(p => new {p.SKU, p.Description, p.Price});
+                    m.ToTable("Product", "BazaDeDate");
+                })
+                .Map(m =>
+                {
+                    m.Properties(p => new {p.SKU, p.ImageUrl});
+                    m.ToTable("ProductWebInfo", "BazaDeDate");
+                });
+        }
+    }
+}
