5a15778a3727cfbfae6c4e9e2c8ee8bfd3e37226
HManuela <manuela.horduna@gmail.com>
2020-04-01 14:00:03 +0300

    Add files via upload

---
+++ Project_2/ObjectWCF/IMyPhotos.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.MyPhotosModel;
+using MyPhotos;
+
+namespace ObjectWCF
+{
+    public class IMyPhotos
+    {
+        interface IMyPhotos
+        {
+            User FindUser(string nume, string prenume, string phoneNumber);
+            
+            void AddUser(User user);
+
+            List<User> GetAllUsers();
+
+            void AddImage(Photos image);
+
+            User FindUserById(int id);
+
+            Photos FindPhotoById(int id);
+
+            void UpdatePhoto(Photos image);
+
+            void DeletePhotos(int id);
+
+            bool ExistsPhoto(int id);
+
+
+
+            
+        }
+    }
+}
---
+++ Project_2/ObjectWCF/MyPhotos.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using MyPhotos;
+using MyPhotos.Api;
+
+namespace ObjectWCF
+{
+    public class MyPhotos : IMyPhotos
+    {
+        private readonly MyPhotos _api;
+
+        public MyPhotos()
+        {
+            _api = new MyPhotosApi();
+        }
+
+        public User FindCustomer(string nume, string prenume, string phoneNumber)
+        {
+            return _api.FindUsers(nume, prenume, phoneNumber);
+        }
+
+        public void AddUser(User user)
+        {
+            _api.AddUser(user);
+        }
+
+        public List<Client> GetAllUsers()
+        {
+            return _api.GetAllUsers();
+        }
+
+
+        public void AddImage(Imagine image)
+        {
+            _api.AddImage(image);
+        }
+
+
+        public Material FindUserById(int id)
+        {
+            return _api.FindUserById(id);
+        }
+
+        public Operatie FindPhotoById(int id)
+        {
+            return _api.FindPhotoById(id);
+        }
+
+
+        void UpdatePhoto(Photos image)
+        {
+            return _api.UpdatePhoto(image);
+        }
+
+        void DeletePhotos(int id)
+        {
+            return _api.DeletePhotos(id);
+
+        }
+
+        bool ExistsPhoto(int id)
+        {
+            return _api.ExistsPhoto(id);
+        }
+
+    }
+}
